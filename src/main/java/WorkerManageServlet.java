16c182c6e6c1bb88ac8fa70dc362be3b7919267a
vijaykarunanithi <2000270E@student.tp.edu.sg>
2022-02-15 18:46:23 +0800

    updated

--- src/main/java/WorkerManageServlet.java
+++ src/main/java/WorkerManageServlet.java
@@ -1,5 +1,6 @@
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,8 +24,7 @@ public class WorkerManageServlet extends HttpServlet {
 	private String jdbcURL = "jdbc:mysql://localhost:3306/workerdetails";
 	private String jdbcUsername = "root";
 	private String jdbcPassword = "password";
-	// Step 2: Prepare list of SQL prepared statements to perform CRUD to our
-	// database
+
 	private static final String INSERT_WORKERS_SQL = "INSERT INTO WorkerDetails"
 			+ " (name, date, gender, phone, role, type) VALUES " + " (?, ?, ?,?,?);";
 	private static final String SELECT_WORKER_BY_ID = "select name,date,gender,phone,role,type from WorkerDetails where name =?";
@@ -32,8 +32,6 @@ public class WorkerManageServlet extends HttpServlet {
 	private static final String DELETE_WORKERS_SQL = "delete from WorkerDetails where name = ?;";
 	private static final String UPDATE_WORKERS_SQL = "update WorkerDetails set name = ?,date= ?, gender =?, phone=?, role =?, type =? where name = ?;";
 
-	// Step 3: Implement the getConnection method which facilitates connection to
-	// the database via JDBC
 	protected Connection getConnection() {
 		Connection connection = null;
 		try {
@@ -111,8 +109,6 @@ public class WorkerManageServlet extends HttpServlet {
 		request.getRequestDispatcher("/workerManagement.jsp").forward(request, response);
 	}
 
-	// method to get parameter, query database for existing user data and redirect
-	// to user edit page
 	private void showEditForm(HttpServletRequest request, HttpServletResponse response)
 			throws SQLException, ServletException, IOException {
 		// get parameter passed in the URL
@@ -145,31 +141,31 @@ public class WorkerManageServlet extends HttpServlet {
 
 	// method to update the user table base on the form data
 	private void updateWorker(HttpServletRequest request, HttpServletResponse response)
-	throws SQLException, IOException {
-	//Step 1: Retrieve value from the request
-	String oriName = request.getParameter("oriName");
-	 String name = request.getParameter("name");
-	 String date = request.getParameter("date");
-	 String gender = request.getParameter("gender");
-	 String phone = request.getParameter("phone");
-	 String role = request.getParameter("role");
-	 String type = request.getParameter("type");
+			throws SQLException, IOException {
+		// Step 1: Retrieve value from the request
+		String oriName = request.getParameter("oriName");
+		String name = request.getParameter("name");
+		String date = request.getParameter("date");
+		String gender = request.getParameter("gender");
+		String phone = request.getParameter("phone");
+		String role = request.getParameter("role");
+		String type = request.getParameter("type");
 
-
-	 //Step 2: Attempt connection with database and execute update user SQL query
-	 try (Connection connection = getConnection(); PreparedStatement statement =
-	connection.prepareStatement(UPDATE_WORKERS_SQL);) {
-	 statement.setString(1, name);
-	 statement.setString(2, date);
-	 statement.setString(3, gender);
-	 statement.setString(4, phone);
-	 statement.setString(5, role);
-	 statement.setString(6, type);
-	 statement.setString(7, oriName);
-	 int i = statement.executeUpdate();
-	 }
-	 //Step 3: redirect back to UserServlet (note: remember to change the url to your project name)
-	 response.sendRedirect("http://localhost:8090/DevOpsProj2/WorkerManageServlet/dashboard");
+		// Step 2: Attempt connection with database and execute update user SQL query
+		try (Connection connection = getConnection();
+				PreparedStatement statement = connection.prepareStatement(UPDATE_WORKERS_SQL);) {
+			statement.setString(1, name);
+			statement.setString(2, date);
+			statement.setString(3, gender);
+			statement.setString(4, phone);
+			statement.setString(5, role);
+			statement.setString(6, type);
+			statement.setString(7, oriName);
+			int i = statement.executeUpdate();
+		}
+		// Step 3: redirect back to UserServlet (note: remember to change the url to
+		// your project name)
+		response.sendRedirect("http://localhost:8090/DevOpsProj2/WorkerManageServlet/dashboard");
 	}
 
 	private void deleteWorker(HttpServletRequest request, HttpServletResponse response)
